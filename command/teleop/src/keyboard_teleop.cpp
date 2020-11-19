fc5568b168e5299e18a7343e0ba9ace2cbff693a
Alexander Koenig <awckoenig@gmail.com>
2020-11-19 20:13:47 +0100

    Isolate ReflexCommander class

--- command/teleop/src/keyboard_teleop.cpp
+++ command/teleop/src/keyboard_teleop.cpp
@@ -2,24 +2,20 @@
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <geometry_msgs/TransformStamped.h>
-#include <std_msgs/Float64.h>
-#include <std_srvs/SetBool.h>
-#include <reflex_msgs/PoseCommand.h>
 
 #include <iostream>
 #include <map>
 #include <termios.h>
 
-std::string node_name = "keyboard_teleop";
+#include "reflex_commander.h"
+
+std::string node_name = "teleop_node";
 std::string source_frame = "world";
 std::string target_frame = "reflex";
-std::string pos_cmd_topic = "reflex/pos_cmd";
-std::string vel_cmd_topic = "reflex/vel_cmd";
-std::string torque_cmd_topic = "reflex/torque_cmd";
-std::string open_srv_name = "reflex/open_hand";
-std::string close_srv_name = "reflex/close_hand";
-double trans_scaling = 0.01;
-double rot_scaling = 0.05;
+
+float trans_scaling = 0.01;
+float rot_scaling = 0.05;
+float finger_scaling = 0.1;
 
 // keys for wrist teleoperation (note this is for my german keyboard)
 std::map<char, std::vector<float>> wrist_bindings{
@@ -37,6 +33,20 @@ std::map<char, std::vector<float>> wrist_bindings{
     {'J', {0, 0, 0, 1, 0, 0}},
     {'K', {0, 0, 0, 0, 1, 0}},
     {'L', {0, 0, 0, 0, 0, 1}},
+};
+
+// keys for wrist teleoperation (note this is for my german keyboard)
+std::map<char, std::vector<float>> finger_bindings{
+
+    // format: {f1, f2, f3, preshape}
+    {'q', {1, 0, 0, 0}},
+    {'w', {0, 1, 0, 0}},
+    {'e', {0, 0, 1, 0}},
+    {'r', {0, 0, 0, 1}},
+    {'Q', {-1, 0, 0, 0}},
+    {'W', {0, -1, 0, 0}},
+    {'E', {0, 0, -1, 0}},
+    {'R', {0, 0, 0, -1}},
 };
 
 // BEGIN CODE FROM https://github.com/methylDragon/teleop_twist_keyboard_cpp/blob/master/src/teleop_twist_keyboard.cpp
@@ -72,114 +82,12 @@ int getch(void)
 
 // END CODE FROM https://github.com/methylDragon/teleop_twist_keyboard_cpp/blob/master/src/teleop_twist_keyboard.cpp
 
-// Class to send predefined, high-level motion primitives (e.g. open/close) to Reflex
-class ReflexPrimitives
-{
-private:
-    ros::Publisher pub;
-    ros::ServiceServer open_service;
-    ros::ServiceServer close_service;
-    reflex_msgs::PoseCommand pos_cmd;
-    float positions[4];
-
-public:
-    ReflexPrimitives(ros::NodeHandle *nh)
-    {
-        pub = nh->advertise<reflex_msgs::PoseCommand>(pos_cmd_topic, 1);
-        open_service = nh->advertiseService(open_srv_name, &ReflexPrimitives::callbackOpen, this);
-        close_service = nh->advertiseService(close_srv_name, &ReflexPrimitives::callbackClose, this);
-    }
-
-    enum Primitive
-    {
-        Open,
-        Close
-    };
-
-    std::string arrayToString(float array[], int numEntries)
-    {
-        std::string str;
-        for (int i = 0; i < numEntries; i++)
-        {
-            str += std::to_string(i) + " ";
-        }
-        return str;
-    }
-
-    std::string executePrimitive(Primitive primitive)
-    {
-        switch (primitive)
-        {
-        case Open:
-        {
-            positions[0] = 0.0;
-            positions[1] = 0.0;
-            positions[2] = 0.0;
-            positions[3] = 0.0;
-            break;
-        }
-        case Close:
-        {
-            positions[0] = 2.5;
-            positions[1] = 2.5;
-            positions[2] = 2.5;
-            positions[3] = 0.0;
-            break;
-        }
-        }
-        ReflexPrimitives::sendCommands(positions);
-        return "Sent commands " + arrayToString(positions, 4) + " to " + pos_cmd_topic;
-    }
-
-    bool callbackOpen(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
-    {
-        if (req.data)
-        {
-            std::string msg = ReflexPrimitives::executePrimitive(Open);
-            res.success = true;
-            res.message = msg;
-        }
-        else
-        {
-            res.success = false;
-            res.message = "Not sending commands.";
-        }
-        return true;
-    }
-
-    bool callbackClose(std_srvs::SetBool::Request &req, std_srvs::SetBool::Response &res)
-    {
-        if (req.data)
-        {
-            std::string msg = ReflexPrimitives::executePrimitive(Close);
-            res.success = true;
-            res.message = msg;
-        }
-        else
-        {
-            res.success = false;
-            res.message = "Not sending commands.";
-        }
-        return true;
-    }
-
-    void sendCommands(float positions[4])
-    {
-        pos_cmd.f1 = positions[0];
-        pos_cmd.f2 = positions[1];
-        pos_cmd.f3 = positions[2];
-        pos_cmd.preshape = positions[3];
-
-        pub.publish(pos_cmd);
-    }
-};
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, node_name);
     ros::NodeHandle nh;
     ros::Rate rate(100);
-    ReflexPrimitives rc = ReflexPrimitives(&nh);
+    ReflexCommander rc = ReflexCommander(&nh);
     ROS_INFO("Launched %s node.", node_name.c_str());
 
     static tf2_ros::TransformBroadcaster br;
@@ -208,8 +116,8 @@ int main(int argc, char **argv)
     while (ros::ok())
     {
         key = getch();
-
-        // teleop of wrist
+        
+        // WRIST CONTROL --------------------------------------------------------
         if (wrist_bindings.count(key) == 1)
         {
             ts.transform.translation.x += trans_scaling * wrist_bindings[key][0];
@@ -231,19 +139,27 @@ int main(int argc, char **argv)
         ts.header.stamp = ros::Time::now();
         br.sendTransform(ts);
 
-        // teleop of individual fingers
+        // FINGER CONTROL (TELEOP) ----------------------------------------------
+        if (finger_bindings.count(key) == 1)
+        {
+            float increment[4] = {finger_scaling * finger_bindings[key][0],
+                                  finger_scaling * finger_bindings[key][1],
+                                  finger_scaling * finger_bindings[key][2],
+                                  finger_scaling * finger_bindings[key][3]};
+            rc.updatePosIncrement(increment);
+        }
+        rc.sendCommands();
 
-        // primitive movement of reflex
+        // FINGER CONTROL (PRIMITIVES) ------------------------------------------
         switch (key)
         {
-        case 'w':
+        case 'y':
         {
             ROS_INFO("Sent Reflex close command.");
             rc.executePrimitive(rc.Primitive::Close);
             break;
         }
-
-        case 'e':
+        case 'x':
         {
             ROS_INFO("Sent Reflex open command.");
             rc.executePrimitive(rc.Primitive::Open);
@@ -251,10 +167,10 @@ int main(int argc, char **argv)
         }
         }
 
-        // terminate upon ctrl-C
+        // EXIT ------------------------------------------
         if (key == '\x03')
         {
-            ROS_INFO("Shutting down...");
+            ROS_INFO("Pressed Ctrl-C. Shutting down...");
             break;
         }
         ros::spinOnce();
