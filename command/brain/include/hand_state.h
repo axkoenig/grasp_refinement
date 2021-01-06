0653f4d9367160b897c8bb041689855f110cf5fa
Alexander Koenig <awckoenig@gmail.com>
2021-01-06 11:51:13 -0500

    Outsource HandState class

---
+++ command/brain/include/hand_state.h
@@ -0,0 +1,47 @@
+#ifndef HAND_STATE_H
+#define HAND_STATE_H
+
+#include <array>
+#include <boost/array.hpp>
+
+class FingerState
+{
+private:
+    int finger_id;
+    const int num_sensors = 9;
+    std::array<bool, 9> sensor_contacts;
+    std::array<float, 9> pressure;
+    float proximal_angle;
+    float distal_angle;
+
+public:
+    FingerState(int finger_id);
+    void setProximalAngle(float proximal_angle);
+    void setDistalAngle(float distal_angle);
+    void setSensorContacts(boost::array<unsigned char, 9> sensor_contacts);
+    void setSensorPressure(boost::array<float, 9> pressure);
+    bool hasContact();
+    bool hasProximalContact();
+    bool hasDistalContact();
+};
+
+class HandState
+{
+public:
+    enum State
+    {
+        NoContact,
+        SingleFingerContact,
+        MultipleFingerContact
+    };
+    State cur_state;
+    const int num_fingers = 3;
+    const int num_motors = 4;
+    FingerState finger_states[3] = {FingerState(1), FingerState(2), FingerState(3)};
+
+    void updateState();
+    State getCurrentState();
+    int countFingersInContact();
+};
+
+#endif
---
+++ command/brain/src/hand_state.cpp
@@ -0,0 +1,106 @@
+#include "hand_state.h"
+
+FingerState::FingerState(int finger_id)
+{
+    proximal_angle = 0.0;
+    distal_angle = 0.0;
+    this->finger_id = finger_id;
+}
+
+void FingerState::setProximalAngle(float proximal_angle)
+{
+    this->proximal_angle = proximal_angle;
+}
+
+void FingerState::setDistalAngle(float distal_angle)
+{
+    this->distal_angle = distal_angle;
+}
+
+void FingerState::setSensorContacts(boost::array<unsigned char, 9> sensor_contacts)
+{
+    for (int i = 0; i < num_sensors; i++)
+    {
+        this->sensor_contacts[i] = sensor_contacts[i];
+    }
+}
+
+void FingerState::setSensorPressure(boost::array<float, 9> pressure)
+{
+    for (int i = 0; i < num_sensors; i++)
+    {
+        this->pressure[i] = pressure[i];
+    }
+}
+
+bool FingerState::hasContact()
+{
+    for (int i = 0; i < num_sensors; i++)
+    {
+        if (sensor_contacts[i] == true)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool FingerState::hasProximalContact()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (sensor_contacts[i] == true)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool FingerState::hasDistalContact()
+{
+    for (int i = 5; i < num_sensors; i++)
+    {
+        if (sensor_contacts[i] == true)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void HandState::updateState()
+{
+    int contact_count = countFingersInContact();
+    switch (contact_count)
+    {
+    case 0:
+        cur_state = NoContact;
+        break;
+    case 1:
+        cur_state = SingleFingerContact;
+        break;
+    default:
+        cur_state = MultipleFingerContact;
+        break;
+    }
+}
+
+HandState::State HandState::getCurrentState()
+{
+    updateState();
+    return cur_state;
+}
+
+int HandState::countFingersInContact()
+{
+    int count = 0;
+    for (int i = 0; i < num_fingers; i++)
+    {
+        if (finger_states[i].hasContact() == true)
+        {
+            count++;
+        }
+    }
+    return count;
+}
