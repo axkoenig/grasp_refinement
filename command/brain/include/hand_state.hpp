b579297cca54235a1a824dc6a9579ad853d06c0c
Alexander Koenig <awckoenig@gmail.com>
2021-01-11 11:58:51 -0500

    Calculate surface normals

--- command/brain/include/hand_state.hpp
+++ command/brain/include/hand_state.hpp
@@ -4,44 +4,58 @@
 #include <array>
 #include <boost/array.hpp>
 
+#include <tf2/LinearMath/Vector3.h>
+#include <reflex_msgs/Hand.h>
+
 class FingerState
 {
 private:
     int finger_id;
     const int num_sensors = 9;
     std::array<bool, 9> sensor_contacts;
-    std::array<float, 9> pressure;
-    float proximal_angle;
-    float distal_angle;
+    std::array<float, 9> sensor_pressure;
+    float proximal_angle = 0.0;
+    float distal_angle = 0.0;
+    float preshape_angle = 0.0;
+    tf2::Vector3 proximal_normal;
+    tf2::Vector3 distal_normal;
+    void updateNormals();
 
 public:
     FingerState(int finger_id);
-    void setProximalAngleFromMsg(float proximal_angle);
-    void setDistalAngleFromMsg(float distal_angle);
+    void setProximalAngleFromMsg(float angle);
+    void setDistalAngleFromMsg(float angle);
+    void setPreshapeAngleFromMsg(float angle);
     void setSensorContactsFromMsg(boost::array<unsigned char, 9> sensor_contacts);
-    void setSensorPressureFromMsg(boost::array<float, 9> pressure);
+    void setSensorPressureFromMsg(boost::array<float, 9> sensor_pressure);
     bool hasContact();
     bool hasProximalContact();
     bool hasDistalContact();
+    tf2::Vector3 getProximalNormal();
+    tf2::Vector3 getDistalNormal();
 };
 
 class HandState
 {
 public:
-    enum State
+    enum ContactState
     {
         NoContact,
         SingleFingerContact,
         MultipleFingerContact
     };
-    State cur_state;
+    
+    ContactState getContactState();
+    int countFingersInContact();
+    void setFingerStateFromMsg(const reflex_msgs::Hand &msg);
+
+private:
+    ContactState cur_state;
+    void updateState();
     const int num_fingers = 3;
     const int num_motors = 4;
     FingerState finger_states[3] = {FingerState(1), FingerState(2), FingerState(3)};
 
-    void updateState();
-    State getCurrentState();
-    int countFingersInContact();
 };
 
 #endif
--- command/brain/src/hand_state.cpp
+++ command/brain/src/hand_state.cpp
@@ -1,3 +1,8 @@
+#include <tf2/LinearMath/Transform.h>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 #include "hand_state.hpp"
 
 FingerState::FingerState(int finger_id)
@@ -7,14 +12,19 @@ FingerState::FingerState(int finger_id)
     this->finger_id = finger_id;
 }
 
-void FingerState::setProximalAngleFromMsg(float proximal_angle)
+void FingerState::setProximalAngleFromMsg(float angle)
 {
-    this->proximal_angle = proximal_angle;
+    proximal_angle = angle;
 }
 
-void FingerState::setDistalAngleFromMsg(float distal_angle)
+void FingerState::setDistalAngleFromMsg(float angle)
 {
-    this->distal_angle = distal_angle;
+    distal_angle = angle;
+}
+
+void FingerState::setPreshapeAngleFromMsg(float angle)
+{
+    preshape_angle = angle;
 }
 
 void FingerState::setSensorContactsFromMsg(boost::array<unsigned char, 9> sensor_contacts)
@@ -25,11 +35,11 @@ void FingerState::setSensorContactsFromMsg(boost::array<unsigned char, 9> sensor
     }
 }
 
-void FingerState::setSensorPressureFromMsg(boost::array<float, 9> pressure)
+void FingerState::setSensorPressureFromMsg(boost::array<float, 9> sensor_pressure)
 {
     for (int i = 0; i < num_sensors; i++)
     {
-        this->pressure[i] = pressure[i];
+        this->sensor_pressure[i] = sensor_pressure[i];
     }
 }
 
@@ -69,6 +79,71 @@ bool FingerState::hasDistalContact()
     return false;
 }
 
+void FingerState::updateNormals()
+{
+    geometry_msgs::TransformStamped ts_msg;
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
+    try
+    {
+        // NOTE: we get transform from reflex base to center of proximal joint from the ROS tf tree.
+        // It will also be available when we run on the real robot. The transforms on the ROS tf tree
+        // are not modified by Gazebo when robot is moved in simulation.
+        std::string proximal_name = "proximal_" + std::to_string(finger_id);
+        ts_msg = tfBuffer.lookupTransform("shell", proximal_name, ros::Time(0));
+    }
+    catch (tf2::TransformException &ex)
+    {
+        ROS_WARN("%s", ex.what());
+    }
+
+    // convert msg to transform stamped
+    tf2::Stamped<tf2::Transform> prox_joint_frame;
+    tf2::fromMsg(ts_msg, prox_joint_frame);
+
+    // rotate proximal joint angle around negative y axis
+    tf2::Quaternion q;
+    q.setRPY(0, -proximal_angle, 0);
+    tf2::Transform rotate_prox_angle = tf2::Transform(q, tf2::Vector3{0, 0, 0});
+    prox_joint_frame *= rotate_prox_angle;
+
+    // take into account preshape rotation for fingers 1 and 2
+    if (finger_id == 1 || finger_id == 2)
+    {
+        // rotate around z axis (negative for finger 1, positive for finger 2)
+        (finger_id == 1) ? q.setRPY(0, 0, -preshape_angle) : q.setRPY(0, 0, preshape_angle);
+        tf2::Transform rotate_preshape_angle = tf2::Transform(q, tf2::Vector3{0, 0, 0});
+        prox_joint_frame *= rotate_preshape_angle;
+    }
+
+    // proximal normal is updated z axis of prox joint frame as it is normal to proximal pad surface by default (see RViz)
+    proximal_normal = prox_joint_frame * tf2::Vector3{0, 0, 1};
+
+    // rotate distal joint angle around negative y axis
+    q.setRPY(0, -distal_angle, 0);
+    tf2::Transform rotate_distal_angle = tf2::Transform(q, tf2::Vector3{0, 0, 0});
+    prox_joint_frame *= rotate_prox_angle;
+
+    // distal normal is updated z axis of prox joint frame
+    distal_normal = prox_joint_frame * tf2::Vector3{0, 0, 1};
+
+    ROS_INFO_STREAM("Finger " << finger_id << " proximal_n: " << proximal_normal);
+    ROS_INFO_STREAM("Finger " << finger_id << " distal_n: " << distal_normal);
+}
+
+tf2::Vector3 FingerState::getProximalNormal()
+{
+    updateNormals();
+    return proximal_normal;
+}
+
+tf2::Vector3 FingerState::getDistalNormal()
+{
+    updateNormals();
+    return distal_normal;
+}
+
 void HandState::updateState()
 {
     int contact_count = countFingersInContact();
@@ -86,7 +161,7 @@ void HandState::updateState()
     }
 }
 
-HandState::State HandState::getCurrentState()
+HandState::ContactState HandState::getContactState()
 {
     updateState();
     return cur_state;
@@ -103,4 +178,21 @@ int HandState::countFingersInContact()
         }
     }
     return count;
+}
+
+void HandState::setFingerStateFromMsg(const reflex_msgs::Hand &msg)
+{
+    for (int i = 0; i < num_fingers; i++)
+    {
+        finger_states[i].setProximalAngleFromMsg(msg.finger[i].proximal);
+        finger_states[i].setDistalAngleFromMsg(msg.finger[i].distal_approx);
+        finger_states[i].setSensorContactsFromMsg(msg.finger[i].contact);
+        finger_states[i].setSensorPressureFromMsg(msg.finger[i].pressure);
+
+        if (i != 2)
+        {
+            // set preshape angle for fingers 1 and 2 (finger 3 doesn't have a preshape angle)
+            finger_states[i].setPreshapeAngleFromMsg(msg.motor[3].joint_angle / 2);
+        }
+    }
 }
