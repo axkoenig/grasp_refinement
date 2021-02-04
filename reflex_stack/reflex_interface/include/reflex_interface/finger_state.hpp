26355bf513bdb2661862807a4ed437bcda7027fa
Alexander Koenig <awckoenig@gmail.com>
2021-02-04 11:48:26 -0500

    Node handle member variable and make vars public

--- reflex_stack/reflex_interface/include/reflex_interface/finger_state.hpp
+++ reflex_stack/reflex_interface/include/reflex_interface/finger_state.hpp
@@ -2,8 +2,10 @@
 #define FINGER_STATE_H
 
 #include <array>
+#include <vector>
 #include <boost/array.hpp>
 
+#include <ros/ros.h>
 #include <tf2/LinearMath/Vector3.h>
 #include <tf2/LinearMath/Transform.h>
 
@@ -11,21 +13,23 @@ class FingerState
 {
 private:
     int finger_id;
-    const int num_sensors = 9;
+    ros::NodeHandle nh;
     std::array<bool, 9> sensor_contacts;
-    std::array<float, 9> sensor_pressure;
+    std::array<float, 9> sensor_pressures;
     float proximal_angle = 0.0;
     float distal_angle = 0.0;
     float preshape_angle = 0.0; // is only modified for fingers 1 and 2
     tf2::Vector3 proximal_normal = tf2::Vector3{0, 0, 0};
     tf2::Vector3 distal_normal = tf2::Vector3{0, 0, 0};
-    tf2::Transform proximal_joint_frame; 
+    tf2::Transform proximal_joint_frame;
+    std::vector<tf2::Transform> contact_frames_world = {};
     void setProximalJointFrame();
     void updateNormalsFromMeasuredJointAngles();
     void updateNormalsExactSim();
 
 public:
-    FingerState(int finger_id);
+    const int num_sensors = 9;
+    FingerState(ros::NodeHandle *nh, int finger_id);
     void setProximalAngleFromMsg(float angle);
     void setDistalAngleFromMsg(float angle);
     void setPreshapeAngleFromMsg(float angle);
@@ -42,8 +46,6 @@ public:
     bool hasContact();
     bool hasProximalContact();
     bool hasDistalContact();
-    tf2::Vector3 getProximalNormal();
-    tf2::Vector3 getDistalNormal();
 };
 
 #endif
