f5b92920c06a4649fda92f18f061a27f95b8fcf3
Alexander Koenig <awckoenig@gmail.com>
2021-02-20 08:29:07 -0500

    Add helper functions for homogeneous matrices

--- agent/src/envs/helpers.py
+++ agent/src/envs/helpers.py
@@ -1,4 +1,17 @@
 import numpy as np
+import tf
+
+
+def get_homo_matrix_from_tq(t, q):
+    mat_t = tf.transformations.translation_matrix(t)
+    mat_q = tf.transformations.quaternion_matrix(q)
+    return np.dot(mat_t, mat_q)
+
+
+def get_tq_from_homo_matrix(mat):
+    t = tf.transformations.translation_from_matrix(mat)
+    q = tf.transformations.quaternion_from_matrix(mat)
+    return t, q
 
 
 def rad2deg(rad):
