c70c0985cdf387ea317612084ca71f0fd45749d2
Alexander Koenig <awckoenig@gmail.com>
2021-03-16 12:57:27 -0400

    Use new message and space API

--- agent/src/envs/refinement_epsilon.py
+++ agent/src/envs/refinement_epsilon.py
@@ -3,7 +3,8 @@ import numpy as np
 
 import rospy
 from std_msgs.msg import Float64, Int32
-from reflex_msgs.msg import PoseCommand, Hand
+from reflex_interface.msg import HandStateStamped
+from reflex_msgs.msg import PoseCommand
 from stable_baselines3.common.callbacks import BaseCallback
 
 from .helpers import rad2deg, deg2rad, get_homo_matrix_from_tq, get_tq_from_homo_matrix
@@ -20,13 +21,23 @@ class ObservationSpace(Space):
         self.num_fingers = 3
         self.num_motors = 4
         self.num_sensors = 9
-        self.num_contact_pressures = self.num_fingers * self.num_sensors
-        self.num_wrist_obj_dists = 3
+        self.num_tot_sensors = self.num_fingers * self.num_sensors
+        self.vars_per_finger = 4 + 2 * self.num_motors
         self.prox_angle_max = 3
 
-        self.add_variable(self.num_motors, "prox_angle", 0, 0, self.prox_angle_max)
-        self.add_variable(self.num_fingers, "dist_angle", 0, 0, 0.2 * self.prox_angle_max)
-        self.add_variable(self.num_contact_pressures, "contact_pressure", 0, 0, 10)
+        for i in range(self.num_fingers):
+            id_str = "_f" + str(i + 1)
+            self.add_variable(1, "prox_angle" + id_str, 0, 0, self.prox_angle_max)
+            self.add_variable(1, "dist_angle" + id_str, 0, 0, 0.2 * self.prox_angle_max)
+            self.add_variable(1, "prox_contact_normal" + id_str, [0, 0, 0], [-1, -1, -1], [1, 1, 1])
+            self.add_variable(1, "dist_contact_normal" + id_str, [0, 0, 0], [-1, -1, -1], [1, 1, 1])
+
+            for j in range(self.num_sensors):
+                id_str = "_f" + str(i + 1) + "_s" + str(j + 1)
+                self.add_variable(1, "sensor_pressure" + id_str, 0, 0, 10)
+                self.add_variable(1, "tactile_positions" + id_str, [0, 0, 0], [-0.2, -0.16, 0.06], [0.2, 0.16, 0.2])
+
+        self.add_variable(1, "preshape_angle", 0, 0, self.prox_angle_max)
 
 
 class ActionSpace(Space):
@@ -93,7 +104,7 @@ class GazeboEnv(gym.Env):
 
         rospy.init_node("agent", anonymous=True)
 
-        self.gazebo_interface = GazeboInterface()
+        self.gi = GazeboInterface()
 
         self.wrist_init_pose = get_homo_matrix_from_tq(
             [-0.020000008416459117, 0.037719972837563544, 0.03613883173559584],
@@ -115,10 +126,10 @@ class GazeboEnv(gym.Env):
 
         self.hand_pub = rospy.Publisher("reflex/pos_cmd", PoseCommand, queue_size=5)
         self.reward_pub = rospy.Publisher("agent/reward", Float64, queue_size=5)
-        self.hand_sub = rospy.Subscriber("reflex/hand_state", Hand, self.hand_callback, queue_size=5)
         self.num_contacts_sub = rospy.Subscriber("reflex/num_contacts", Int32, self.num_contacts_callback, queue_size=5)
         self.epsilon_force_sub = rospy.Subscriber("reflex/epsilon_force", Float64, self.epsilon_force_callback, queue_size=5)
         self.epsilon_torque_sub = rospy.Subscriber("reflex/epsilon_torque", Float64, self.epsilon_torque_callback, queue_size=5)
+        self.hand_state_sub = rospy.Subscriber("reflex/ri_hand_state", HandStateStamped, self.hand_state_callback, queue_size=5)
 
         self.num_contacts = 0
         self.epsilon_force = 0
@@ -137,31 +148,50 @@ class GazeboEnv(gym.Env):
     def epsilon_torque_callback(self, msg):
         self.epsilon_torque = msg.data
 
-    def hand_callback(self, msg):
+    def hand_state_callback(self, msg):
+
+        self.obs.set_cur_val_by_name("preshape_angle", msg.preshape_angle)
 
         for i in range(self.obs.num_fingers):
-            # joint pos 1 to 3 from magnetic encoders (more accurate than motor encoders)
-            self.obs.vars[i].cur_val = msg.finger[i].proximal
-            self.obs.vars[i + self.obs.num_motors].cur_val = msg.finger[i].distal_approx
+            id_str = "_f" + str(i + 1)
+            self.obs.set_cur_val_by_name("prox_angle" + id_str, msg.finger_state[i].proximal_angle)
+            self.obs.set_cur_val_by_name("dist_angle" + id_str, msg.finger_state[i].distal_angle)
 
+            # normals
+            if msg.finger_state[i].prox_in_contact:
+                normal = self.gi.ros_vector_to_list(msg.finger_state[i].prox_normal)
+                self.obs.set_cur_val_by_name("prox_contact_normal" + id_str, normal)
+            else:
+                self.obs.set_cur_val_by_name("prox_contact_normal" + id_str, [0, 0, 0])
+            if msg.finger_state[i].dist_in_contact:
+                normal = self.gi.ros_vector_to_list(msg.finger_state[i].dist_normal)
+                self.obs.set_cur_val_by_name("dist_contact_normal" + id_str, normal)
+            else:
+                self.obs.set_cur_val_by_name("dist_contact_normal" + id_str, [0, 0, 0])
+
+            # tactile feedback
             for j in range(self.obs.num_sensors):
-                self.obs.vars[i + self.obs.num_motors + self.obs.num_fingers + j].cur_val = msg.finger[i].pressure[j]
+                id_str = "_f" + str(i + 1) + "_s" + str(j + 1)
+                self.obs.set_cur_val_by_name("sensor_pressure" + id_str, msg.finger_state[i].sensor_pressure[j])
 
-        # joint pos 4 from motor encoder (there is no magnetic encoder)
-        self.obs.vars[3].cur_val = msg.motor[3].joint_angle
+                if msg.finger_state[i].sensor_contact[j]:
+                    tactile_positions = self.gi.ros_vector_to_list(msg.finger_state[i].tactile_position[j])
+                    self.obs.set_cur_val_by_name("tactile_positions" + id_str, tactile_positions)
+                else:
+                    self.obs.set_cur_val_by_name("tactile_positions" + id_str, [0, 0, 0])
 
     def step(self, action):
 
         if action[0] > 0.5:
-            self.gazebo_interface.regrasp([action[1], action[2], action[3]])
+            self.gi.regrasp([action[1], action[2], action[3]])
         else:
             # run for a short time, and collect new reward
-            self.gazebo_interface.run_for_seconds(0.01)
+            self.gi.run_for_seconds(0.01)
 
         # get object shift and distance to object (used for logging)
-        t_obj, _ = get_tq_from_homo_matrix(self.gazebo_interface.get_object_pose())
+        t_obj, _ = get_tq_from_homo_matrix(self.gi.get_object_pose())
         self.obj_shift = np.linalg.norm(t_obj - self.t_obj_init)
-        self.dist_tcp_obj = self.gazebo_interface.get_dist_tcp_obj()
+        self.dist_tcp_obj = self.gi.get_dist_tcp_obj()
 
         reward = self.epsilon_force + self.epsilon_torque
         reward_msg = Float64(reward)
@@ -170,11 +200,17 @@ class GazeboEnv(gym.Env):
         done = False
         logs = {}
 
+        prox_angles = [
+            self.obs.get_cur_vals_by_name("prox_angle_f1"),
+            self.obs.get_cur_vals_by_name("prox_angle_f2"),
+            self.obs.get_cur_vals_by_name("prox_angle_f3"),
+        ]
+
         # check if should end episode
         if self.obj_shift > self.obj_shift_tol:
             done = True
             rospy.loginfo(f"Object shift is above {self.obj_shift_tol} m. Setting done = True.")
-        elif not all(prox_angle.cur_val < self.joint_lim for prox_angle in self.obs.vars[:3]):
+        elif not all(prox_angle < self.joint_lim for prox_angle in prox_angles):
             done = True
             rospy.loginfo(f"One angle is above {self.joint_lim} rad. Setting done = True.")
         elif rospy.get_rostime().secs - self.last_reset_time.secs > self.max_ep_len:
@@ -184,7 +220,7 @@ class GazeboEnv(gym.Env):
         return self.obs.get_cur_vals(True), reward, done, logs
 
     def reset(self):
-        self.gazebo_interface.reset_world(self.wrist_init_pose, self.obj_init_pose)
+        self.gi.reset_world(self.wrist_init_pose, self.obj_init_pose)
 
         # reset vars
         obs = np.zeros(self.observation_space.shape)
