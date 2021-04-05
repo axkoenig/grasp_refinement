3f66232b3689dbb5a0f39bf4649b44a8447a6839
Alexander Koenig <awckoenig@gmail.com>
2021-04-05 12:58:52 -0400

    Adapt API to new message type

--- reflex_stack/simulation/sensor_listener/include/sensor_listener/reflex_finger.hpp
+++ reflex_stack/simulation/sensor_listener/include/sensor_listener/reflex_finger.hpp
@@ -6,6 +6,7 @@
 #include <tf2/LinearMath/Vector3.h>
 #include <tf2/LinearMath/Transform.h>
 
+#include "sensor_listener/ContactFrame.h"
 #include "sensor_listener/reflex_sensor.hpp"
 
 class ReflexFinger
@@ -39,25 +40,28 @@ private:
     float len_dist_pad = 0.0385;
     tf2::Vector3 prox_link_to_prox_pad_origin = tf2::Vector3{0.035, 0, 0.004};
     tf2::Vector3 dist_link_to_dist_pad_origin = tf2::Vector3{0.01875, 0, 0.007};
-
-public:
-    ReflexSensor sensors[9];
-
-    ReflexFinger(const int &finger_id);
-    void proximal_callback(const control_msgs::JointControllerState &msg);
-    void proximal_to_flex_callback(const control_msgs::JointControllerState &msg);
-    void flex_to_distal_callback(const control_msgs::JointControllerState &msg);
-    float getProximalAngle();
-    float getDistalAngle();
-    float calc_l2_norm(const float (&vector)[3]);
     void proximal_contacts_callback(const gazebo_msgs::ContactsState &msg);
     void distal_contacts_callback(const gazebo_msgs::ContactsState &msg);
     void eval_contacts_callback(const gazebo_msgs::ContactsState &msg,
+                                std::vector<sensor_listener::ContactFrame> &contact_frames,
                                 const int &first_sensor_idx,
                                 const int &num_sensors_on_link,
                                 const float sensor_boundaries[],
                                 const tf2::Transform &world_to_link,
                                 const tf2::Vector3 &link_to_pad_origin);
+    void proximal_callback(const control_msgs::JointControllerState &msg);
+    void proximal_to_flex_callback(const control_msgs::JointControllerState &msg);
+    void flex_to_distal_callback(const control_msgs::JointControllerState &msg);
+
+public:
+    ReflexSensor sensors[9];
+    std::vector<sensor_listener::ContactFrame> prox_contact_frames;
+    std::vector<sensor_listener::ContactFrame> dist_contact_frames;
+    ReflexFinger(const int &finger_id);
+    float getProximalAngle();
+    float getDistalAngle();
+    tf2::Vector3 create_vec_from_msg(const geometry_msgs::Vector3 &msg);
+
 };
 
 #endif
