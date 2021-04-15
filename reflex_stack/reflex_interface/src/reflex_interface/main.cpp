af7a0bc6150ab4ca47684620019da21f979510c2
Alexander Koenig <awckoenig@gmail.com>
2021-04-15 09:47:14 -0400

    Get booleans from ros param server

--- reflex_stack/reflex_interface/src/reflex_interface/main.cpp
+++ reflex_stack/reflex_interface/src/reflex_interface/main.cpp
@@ -2,6 +2,7 @@
 #include <ros/ros.h>
 
 #include "reflex_interface/reflex_interface.hpp"
+#include "gazebo_interface/gazebo_interface.hpp"
 
 std::string node_name = "reflex_interface_node";
 
@@ -11,10 +12,13 @@ int main(int argc, char **argv)
     ROS_INFO("Launched %s.", node_name.c_str());
 
     ros::NodeHandle nh;
-    bool use_sim_data_hand = true;
-    bool use_sim_data_obj = true;
-    ReflexInterface ri = ReflexInterface(&nh, use_sim_data_hand, use_sim_data_obj);
+
+    bool use_sim_data_hand;
+    bool use_sim_data_obj;
+    getParam(&nh, &use_sim_data_hand, "use_sim_data_hand");
+    getParam(&nh, &use_sim_data_obj, "use_sim_data_obj");
     
+    ReflexInterface ri = ReflexInterface(&nh, use_sim_data_hand, use_sim_data_obj);
     ros::MultiThreadedSpinner spinner(0);
     spinner.spin();
 }
