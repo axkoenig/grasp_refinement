95fe42ceffa9aca74137b59d58a3e0f8fd603841
Alexander Koenig <awckoenig@gmail.com>
2021-04-17 16:55:21 -0400

    Outsource callback list

--- agent/src/main.py
+++ agent/src/main.py
@@ -15,14 +15,17 @@ from stable_baselines3.common.env_checker import check_env
 def main(args):
 
     log_path = os.path.join(args.output_dir, "logs", args.environment)
-    ckpt_path =  os.path.join(args.output_dir, "models", args.environment, args.log_name)
+    ckpt_path = os.path.join(args.output_dir, "models", args.environment, args.log_name)
     model_path = os.path.join(ckpt_path, "final_model")
 
     print("Loading environment...")
     if args.environment == "refinement":
         from envs.refinement import GazeboEnv
-        from envs.tb_callback import TensorboardCallback
-        env = GazeboEnv(args.exec_secs, args.max_ep_len, args.joint_lim, args.obj_shift_tol, args.reward_weight, [args.x_error, args.y_error, args.z_error])
+        from envs.callbacks import TensorboardCallback
+
+        env = GazeboEnv(
+            args.exec_secs, args.max_ep_len, args.joint_lim, args.obj_shift_tol, args.reward_weight, [args.x_error, args.y_error, args.z_error]
+        )
     else:
         raise ValueError("Invalid environment name.")
 
@@ -42,14 +45,11 @@ def main(args):
     action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
     # model = TD3(MlpPolicy, env, action_noise=action_noise, verbose=1, tensorboard_log=log_path, seed=args.seed)
     model = TD3(MlpPolicy, env, action_noise=action_noise, verbose=1, tensorboard_log=log_path)
-    
+
     if args.train:
-
-        checkpoint_callback = CheckpointCallback(save_freq=args.chkpt_freq, save_path=ckpt_path, name_prefix="chkpt")
-        rewards_callback = TensorboardCallback()
-
+        callbacks = [CheckpointCallback(save_freq=args.chkpt_freq, save_path=ckpt_path, name_prefix="chkpt"), TensorboardCallback()]
         print("Training model...")
-        model.learn(total_timesteps=args.time_steps, tb_log_name=args.log_name, callback=[checkpoint_callback, rewards_callback])
+        model.learn(total_timesteps=args.time_steps, tb_log_name=args.log_name, callback=callbacks)
         model.save(model_path)
         print("Saved model under: " + model_path)
 
