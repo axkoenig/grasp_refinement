76e4ace2b7c8ee65265024d9cea05f2dfa4aba9c
Alexander Koenig <awckoenig@gmail.com>
2021-04-21 14:27:01 -0400

    Add experiments script

---
+++ cluster/experiments.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# parse arguments
+while [[ "$#" -gt 0 ]]
+do
+case $1 in
+    -n|--num_experiments)
+    NUM_EXPERIMENTS=$2
+    ;;
+esac
+shift
+done
+
+# runs multiple experiments in parallel
+NOW=$(date +"%m-%d-%Y/%T")
+CLUSTER_PATH=$HOME/overlay/work/catkin_ws/src/grasp_refinement/cluster
+echo "Submitting ${NUM_EXPERIMENTS} experiments for each framework..."
+
+submit_job () {
+    LOG_NAME=${NOW}_f${1}_id${i}
+    echo $LOG_NAME
+    sbatch --export=FRAMEWORK=${1} --export=LOG_NAME=${LOG_NAME} CLUSTER_PATH/experiment.slurm
+} 
+
+for i in $(seq 1 $NUM_EXPERIMENTS); 
+do 
+submit_job "1"
+submit_job "2"
+done
+
+echo "Done."
