f8493b2f27e0ba6f2835a0c1db6dd48487065150
Alexander Koenig <awckoenig@gmail.com>
2021-04-21 14:31:01 -0400

    Fix errors

--- cluster/experiments.sh
+++ cluster/experiments.sh
@@ -12,19 +12,20 @@ done
 
 # runs multiple experiments in parallel
 NOW=$(date +"%m-%d-%Y/%T")
+TIME_STEPS=10000
 CLUSTER_PATH=$HOME/overlay/work/catkin_ws/src/grasp_refinement/cluster
 echo "Submitting ${NUM_EXPERIMENTS} experiments for each framework..."
 
 submit_job () {
     LOG_NAME=${NOW}_f${1}_id${i}
     echo $LOG_NAME
-    sbatch --export=FRAMEWORK=${1} --export=LOG_NAME=${LOG_NAME} CLUSTER_PATH/experiment.slurm
+    sbatch --export=FRAMEWORK=${1},TIME_STEPS=${TIME_STEPS} --export=LOG_NAME=${LOG_NAME} CLUSTER_PATH/experiment.slurm
 } 
 
 for i in $(seq 1 $NUM_EXPERIMENTS); 
 do 
-submit_job "1"
-submit_job "2"
+submit_job "1"  # framework 1
+submit_job "2"  # framework 2
 done
 
 echo "Done."
