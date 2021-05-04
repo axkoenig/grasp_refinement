6b0f401678a3661f7139518e15e9b23404e6aaef
Alexander Koenig <awckoenig@gmail.com>
2021-05-04 18:55:58 -0400

    Train id is seed

--- agent/src/train.sh
+++ agent/src/train.sh
@@ -13,9 +13,9 @@ rosclean purge -y
 trap "exit" INT
 for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13
 do
-    # python main.py --framework=1 --log_name=30Apr_F1_Lim3_X0.03_Z0.02_try${i} --time_steps=$time_steps --max_ep_len=15 --x_error_min ${x_error_min} --x_error_max ${x_error_max} --y_error_min ${y_error_min} --y_error_max ${y_error_max} --z_error_min ${z_error_min} --z_error_max ${z_error_max} 
-    # python main.py --framework=2 --log_name=30Apr_F2_Lim3_X0.03_Z0.02_try${i} --time_steps=$time_steps --max_ep_len=15 --x_error_min ${x_error_min} --x_error_max ${x_error_max} --y_error_min ${y_error_min} --y_error_max ${y_error_max} --z_error_min ${z_error_min} --z_error_max ${z_error_max} 
-    python main.py --framework=3 --log_name=03May_F3_Lim3_X0.03_Z0.02_RW1_try${i} --time_steps=$time_steps --max_ep_len=15 --x_error_min ${x_error_min} --x_error_max ${x_error_max} --y_error_min ${y_error_min} --y_error_max ${y_error_max} --z_error_min ${z_error_min} --z_error_max ${z_error_max} 
+    python main.py --framework=1 --seed=${i} --log_name=30Apr_F1_Lim3_X0.03_Z0.02_try${i} --time_steps=$time_steps --max_ep_len=15 --x_error_min ${x_error_min} --x_error_max ${x_error_max} --y_error_min ${y_error_min} --y_error_max ${y_error_max} --z_error_min ${z_error_min} --z_error_max ${z_error_max} 
+    python main.py --framework=2 --seed=${i} --log_name=30Apr_F2_Lim3_X0.03_Z0.02_try${i} --time_steps=$time_steps --max_ep_len=15 --x_error_min ${x_error_min} --x_error_max ${x_error_max} --y_error_min ${y_error_min} --y_error_max ${y_error_max} --z_error_min ${z_error_min} --z_error_max ${z_error_max} 
+    python main.py --framework=3 --seed=${i} --log_name=03May_F3_Lim3_X0.03_Z0.02_RW1_try${i} --time_steps=$time_steps --max_ep_len=15 --x_error_min ${x_error_min} --x_error_max ${x_error_max} --y_error_min ${y_error_min} --y_error_max ${y_error_max} --z_error_min ${z_error_min} --z_error_max ${z_error_max} 
 done
 
 echo "Done! Have a nice day." 
