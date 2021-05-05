99f02d32981a7c9209c37d3e4e5a7d5e2c4cbe48
Alexander Koenig <awckoenig@gmail.com>
2021-05-05 11:07:01 -0400

    Introduce rotational action

--- agent/src/envs/space_act.py
+++ agent/src/envs/space_act.py
@@ -8,10 +8,8 @@ class ActionSpace(Space):
         super().__init__()
 
         self.add_variable(1, "trigger_regrasp", 0, 0, 1)
-        self.add_variable(1, "wrist_incr_x", 0, -0.01, 0.01)
-        self.add_variable(1, "wrist_incr_y", 0, -0.001, 0.001)
-        self.add_variable(1, "wrist_incr_z", 0, -0.005, 0.02)
-        self.add_variable(1, "wrist_pitch", 0, -0.1, 0.1)
+        self.add_variable(3, "wrist_trans", 0, -0.01, 0.01)
+        self.add_variable(3, "wrist_rot", 0, -0.08, 0.08)
         self.add_variable(3, "trigger_finger_tightening", 0, 0, 1)
 
         self.print_num_dimensions()
