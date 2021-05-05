2bf5211a2a002ae56b3b1004169bd840b360ebe2
Alexander Koenig <awckoenig@gmail.com>
2021-05-05 16:02:24 -0400

    Rotate wrist less

--- agent/src/envs/space_act.py
+++ agent/src/envs/space_act.py
@@ -9,7 +9,7 @@ class ActionSpace(Space):
 
         self.add_variable(1, "trigger_regrasp", 0, 0, 1)
         self.add_variable(3, "wrist_trans", 0, -0.01, 0.01)
-        self.add_variable(3, "wrist_rot", 0, -0.08, 0.08)
+        self.add_variable(3, "wrist_rot", 0, -0.05, 0.05)
         self.add_variable(3, "trigger_finger_tightening", 0, 0, 1)
 
         self.print_num_dimensions()
