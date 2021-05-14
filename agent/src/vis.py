193615ed9f1e5892a379146daca68e9cac0b82aa
Alexander Koenig <awckoenig@gmail.com>
2021-05-14 11:40:59 -0400

    Add try catch

--- agent/src/vis.py
+++ agent/src/vis.py
@@ -6,15 +6,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+from tensorboard.backend.event_processing.directory_watcher import DirectoryDeletedError
 
-
-def get_experiment_data(log_path, prefix, framework, try_id, scalar_name, window_size=4, verbose=False):
+def get_experiment_data(log_path, prefix, framework, try_id, scalar_name, window_size=1, cap_len=10000, verbose=False):
 
     # load tensorboard logs
     experiment_path = os.path.join(log_path, f"{prefix}_f{framework}_id{try_id}_1")
-    event_acc = EventAccumulator(experiment_path)
-    event_acc.Reload()
-    world_times, time_steps, vals = zip(*event_acc.Scalars(scalar_name))
+    try: 
+        event_acc = EventAccumulator(experiment_path)
+        event_acc.Reload()
+        try:
+            world_times, time_steps, vals = zip(*event_acc.Scalars(scalar_name))
+        except KeyError:
+            print("Could not find scalar " + scalar_name)
+            return None
+    except DirectoryDeletedError:
+        print(experiment_path+ " does not exist. Returning None.")
+        return None
 
     # smooth data with moving average filter
     vals_smooth = np.convolve(vals, np.ones(window_size), "valid") / window_size
@@ -23,6 +31,10 @@ def get_experiment_data(log_path, prefix, framework, try_id, scalar_name, window
     # linearly interpolate the missing time steps
     dense_time_steps = np.arange(int(time_steps_smooth[0]), int(time_steps_smooth[-1]) + 1)
     dense_vals = np.interp(dense_time_steps, time_steps_smooth, vals_smooth)
+
+    if len(dense_vals) > cap_len:
+        dense_time_steps = dense_time_steps[:cap_len]
+        dense_vals = dense_vals[:cap_len]
 
     # create pd dataframe
     data = {"dense_time_steps": dense_time_steps, scalar_name: dense_vals}
@@ -74,25 +86,25 @@ if __name__ == "__main__":
     parser.add_argument(
         "--log_path",
         type=str,
-        default="/home/parallels/cluster_logs",
+        default="/home/parallels/cluster_logs_rot10",
         help="Path to tensorboard log.",
     )
     parser.add_argument(
         "--prefix",
         type=str,
-        default="05May_MultObjWRot",
+        default="12May_EvalTestWRot10",
         help="Prefix comment of your experiment.",
     )
     parser.add_argument(
         "--scalar_name",
         type=str,
-        default="drop_test/sustained_holding",
+        default="eval/mean_sustained_holding",
         help="Which metric to plot.",
     )
     parser.add_argument(
         "--max_num_trials",
         type=int,
-        default=5,
+        default=14,
         help="How many trials were in your experiment.",
     )
     args = parser.parse_args()
