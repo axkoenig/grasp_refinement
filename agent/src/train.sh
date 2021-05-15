d7be3e09478ccb7fea34034d4bab64f6245f6e08
Alexander Koenig <awckoenig@gmail.com>
2021-05-15 12:48:19 -0400

    New errors

--- agent/src/train.sh
+++ agent/src/train.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # transl offset
-x_error_max=0.05
+x_error_max=0.02
 x_error_min=-$x_error_max
 y_error_max=0.02
 y_error_min=-$y_error_max
-z_error_max=0.05
+z_error_max=0.02
 z_error_min=-$z_error_max
 
 # rot offset
-roll_error_max=15
+roll_error_max=5
 roll_error_min=-$roll_error_max
-pitch_error_max=15
+pitch_error_max=5
 pitch_error_min=-$pitch_error_max
-yaw_error_max=15
+yaw_error_max=5
 yaw_error_min=-$yaw_error_max
 
-time_steps=3000
-prefix="10May_NewControlNewActions_Night"
+time_steps=1000
+prefix="14May_NoDelta_eve"
 
 # occasionally run this
 # rosclean purge -y
@@ -36,7 +36,7 @@ EOM
 trap "exit" INT
 for i in 1 2 3 4 5 6 7 8 9 10; do
     python main.py --framework=1 --seed=${i} --log_name=${prefix}_f1_id${i} $HPARAMS
-    python main.py --framework=2 --seed=${i} --log_name=${prefix}_f2_id${i} $HPARAMS
+    # python main.py --framework=2 --seed=${i} --log_name=${prefix}_f2_id${i} $HPARAMS
     python main.py --framework=3 --seed=${i} --log_name=${prefix}_f3_id${i} $HPARAMS
 done
 
