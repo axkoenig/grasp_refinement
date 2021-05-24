b9ebea5aa4088f1aa9c037f0522c8cb7af82664f
Alexander Koenig <awckoenig@gmail.com>
2021-05-24 17:59:08 -0400

    Sample action from Bernoulli

--- agent/src/envs/space_act.py
+++ agent/src/envs/space_act.py
@@ -28,14 +28,17 @@ class ActionSpace(Space):
         "Maps each value in a list of values in range [self.min_action, self.max_action] to [min, max]"
         if not min < max:
             raise ValueError("Your 'min' must be less than your 'max'.")
-        for i in range(len(vals)):
-            vals[i] = np.interp(vals[i], (self.min_action, self.max_action), (min, max))
-        return vals
+        try: 
+            for i in range(len(vals)):
+                vals[i] = np.interp(vals[i], (self.min_action, self.max_action), (min, max))
+            return vals
+        except TypeError: # we only have one value
+            return np.interp(vals, (self.min_action, self.max_action), (min, max))
 
     def get_action_dict(self, action, verbose=True):
         "Converts action array from gym environment to a more expressive dict with correct ranges"
         action_dict = {
-            "trigger_regrasp": True if action[0] > 0 else False,
+            "trigger_regrasp": np.random.binomial(1, self.map_vals_to_range(0, 1, action[0])),
             "wrist_trans": self.map_vals_to_range(self.min_wrist_trans, self.max_wrist_trans, action[1:4]),
             "wrist_rot": self.map_vals_to_range(self.min_wrist_rot, self.max_wrist_rot, action[4:7]),
             "fingers_incr": self.map_vals_to_range(self.min_finger_incr, self.max_finger_incr, action[7:10]),
@@ -43,5 +46,8 @@ class ActionSpace(Space):
         if verbose:
             rospy.loginfo("--- Actions ---")
             for key, value in action_dict.items():
-                rospy.loginfo(f'- {key:20}{value}')
+                if key == "trigger_regrasp":
+                    rospy.loginfo(f'- {key:20}{value} sampled from: {self.map_vals_to_range(0, 1, action[0])}')
+                else:
+                    rospy.loginfo(f'- {key:20}{value}')
         return action_dict
