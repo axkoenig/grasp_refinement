987ed8529e2fc0e5dfe9edaf8a6ad4ff1d081011
Alexander Koenig <awckoenig@gmail.com>
2021-06-07 13:50:41 -0400

    Add mutexes

--- agent/src/controller/spaces/space.py
+++ agent/src/controller/spaces/space.py
@@ -1,3 +1,5 @@
+from multiprocessing import Lock
+
 import numpy as np
 import rospy
 
@@ -17,6 +19,7 @@ class Space:
 
     def __init__(self):
         self.vars = []
+        self.mutex = Lock()
         self.dim = 0
 
     def add_variable(self, num_instances, name, init_value, min_val, max_val):
@@ -41,39 +44,43 @@ class Space:
         return np.float32(max_vals)
 
     def get_cur_vals(self, verbose=False):
-        cur_vals = np.empty((0,))
-        for i in range(self.dim):
-            if verbose:
-                rospy.loginfo("Cur value of " + self.vars[i].name + f" is {self.vars[i].cur_val}.")
-            val = self.vars[i].cur_val
+        with self.mutex:
+            cur_vals = np.empty((0,))
+            for i in range(self.dim):
+                if verbose:
+                    rospy.loginfo("Cur value of " + self.vars[i].name + f" is {self.vars[i].cur_val}.")
+                val = self.vars[i].cur_val
 
-            # clip value to bounds if necessary
-            if np.any(np.less(val, self.vars[i].min_val)) or np.any(np.greater(val, self.vars[i].max_val)):
-                rospy.logwarn(f"Oops value {val} with name {self.vars[i].name} is out of bounds [{self.vars[i].min_val},{self.vars[i].max_val}]!")
-                val = np.clip(val, self.vars[i].min_val, self.vars[i].max_val)
-                rospy.logwarn(f"Clipped {self.vars[i].name} value is {val}")
+                # clip value to bounds if necessary
+                if np.any(np.less(val, self.vars[i].min_val)) or np.any(np.greater(val, self.vars[i].max_val)):
+                    rospy.logwarn(f"Oops value {val} with name {self.vars[i].name} is out of bounds [{self.vars[i].min_val},{self.vars[i].max_val}]!")
+                    val = np.clip(val, self.vars[i].min_val, self.vars[i].max_val)
+                    rospy.logwarn(f"Clipped {self.vars[i].name} value is {val}")
 
-            cur_vals = np.append(cur_vals, val)
-        return np.float32(cur_vals)
+                cur_vals = np.append(cur_vals, val)
+            return np.float32(cur_vals)
 
     def get_cur_vals_by_name(self, name, verbose=False):
         "Returns a concatenation of all current values that match the given name."
-        cur_vals = np.empty((0,))
-        for i in range(self.dim):
-            if self.vars[i].name == name:
-                cur_vals = np.append(cur_vals, self.vars[i].cur_val)
-        if verbose:
-            rospy.loginfo("Cur values of " + self.__class__.__name__ + f" that match the name {name} are {cur_vals}.")
-        return np.float32(cur_vals)
+        with self.mutex:
+            cur_vals = np.empty((0,))
+            for i in range(self.dim):
+                if self.vars[i].name == name:
+                    cur_vals = np.append(cur_vals, self.vars[i].cur_val)
+            if verbose:
+                rospy.loginfo("Cur values of " + self.__class__.__name__ + f" that match the name {name} are {cur_vals}.")
+            return np.float32(cur_vals)
 
     def set_cur_val_by_name(self, name, cur_val):
-        for i in range(self.dim):
-            if self.vars[i].name == name:
-                self.vars[i].cur_val = cur_val
-                return
-        raise ValueError(f"Variable with name {name} not found.")
+        with self.mutex:
+            for i in range(self.dim):
+                if self.vars[i].name == name:
+                    self.vars[i].cur_val = cur_val
+                    return
+            raise ValueError(f"Variable with name {name} not found.")
 
     def print_all_cur_vals(self):
-        rospy.loginfo("\n=== Cur values of " + self.__class__.__name__ + f" ===")
-        for i in range(self.dim):
-            rospy.loginfo(self.vars[i].name + ": " + str(self.vars[i].cur_val))
+        with self.mutex:
+            rospy.loginfo("\n=== Cur values of " + self.__class__.__name__ + f" ===")
+            for i in range(self.dim):
+                rospy.loginfo(self.vars[i].name + ": " + str(self.vars[i].cur_val))
