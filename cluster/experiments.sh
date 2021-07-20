6723a702eb293baf9b8a03704cd8864597709827
Alexander Koenig <awckoenig@gmail.com>
2021-07-19 18:44:46 -0700

    Seach for reward weights

--- cluster/experiments.sh
+++ cluster/experiments.sh
@@ -44,12 +44,8 @@ PITCH_ERROR_MIN=-$PITCH_ERROR_MAX
 YAW_ERROR_MAX=10
 YAW_ERROR_MIN=-$YAW_ERROR_MAX
 
-# reward weights
-W_EPS_TORQUE=5
-W_DELTA=1
-
 # ports
-NUM_FRAMEWORKS=4
+NUM_FRAMEWORKS=8
 BASE_ROS_PORT=11711
 BASE_GAZEBO_PORT=$(($BASE_ROS_PORT + $NUM_EXPERIMENTS * $NUM_FRAMEWORKS))
 
@@ -58,20 +54,23 @@ EXPERIMENT_COUNTER=0
 submit_job() {
     ROS_PORT=$(($BASE_ROS_PORT + $EXPERIMENT_COUNTER))
     GAZEBO_PORT=$(($BASE_GAZEBO_PORT + $EXPERIMENT_COUNTER))
-    LOG_NAME=${COMMENT}_f${1}_s${4}_id${2}_algo${3}
+    LOG_NAME=${COMMENT}_f${1}_s${4}_id${2}_algo${3}_e${5}_d${6}
     echo "Submitting job with LOG_NAME $LOG_NAME, ROS_PORT $ROS_PORT, GAZEBO_PORT $GAZEBO_PORT"
-    sbatch --output=$LOG_PATH/$LOG_NAME.out --error=$LOG_PATH/$LOG_NAME.err --export=FRAMEWORK=${1},SEED=${2},ALGORITHM=${3},FORCE_SENSING=${4},TIME_STEPS=${TIME_STEPS},W_EPS_TORQUE=${W_EPS_TORQUE},W_DELTA=${W_DELTA},ROS_PORT=${ROS_PORT},GAZEBO_PORT=${GAZEBO_PORT},LOG_NAME=${LOG_NAME},CLUSTER_PATH=$CLUSTER_PATH,HOME_DIR=$HOME_DIR,X_ERROR_MAX=$X_ERROR_MAX,X_ERROR_MIN=$X_ERROR_MIN,Y_ERROR_MAX=$Y_ERROR_MAX,Y_ERROR_MIN=$Y_ERROR_MIN,Z_ERROR_MAX=$Z_ERROR_MAX,Z_ERROR_MIN=$Z_ERROR_MIN,ROLL_ERROR_MAX=$ROLL_ERROR_MAX,ROLL_ERROR_MIN=$ROLL_ERROR_MIN,PITCH_ERROR_MAX=$PITCH_ERROR_MAX,PITCH_ERROR_MIN=$PITCH_ERROR_MIN,YAW_ERROR_MAX=$YAW_ERROR_MAX,YAW_ERROR_MIN=$YAW_ERROR_MIN ${CLUSTER_PATH}/experiment.slurm
+    sbatch --output=$LOG_PATH/$LOG_NAME.out --error=$LOG_PATH/$LOG_NAME.err --export=FRAMEWORK=${1},SEED=${2},ALGORITHM=${3},FORCE_SENSING=${4},TIME_STEPS=${TIME_STEPS},W_EPS_TORQUE=${5},W_DELTA=${6},ROS_PORT=${ROS_PORT},GAZEBO_PORT=${GAZEBO_PORT},LOG_NAME=${LOG_NAME},CLUSTER_PATH=$CLUSTER_PATH,HOME_DIR=$HOME_DIR,X_ERROR_MAX=$X_ERROR_MAX,X_ERROR_MIN=$X_ERROR_MIN,Y_ERROR_MAX=$Y_ERROR_MAX,Y_ERROR_MIN=$Y_ERROR_MIN,Z_ERROR_MAX=$Z_ERROR_MAX,Z_ERROR_MIN=$Z_ERROR_MIN,ROLL_ERROR_MAX=$ROLL_ERROR_MAX,ROLL_ERROR_MIN=$ROLL_ERROR_MIN,PITCH_ERROR_MAX=$PITCH_ERROR_MAX,PITCH_ERROR_MIN=$PITCH_ERROR_MIN,YAW_ERROR_MAX=$YAW_ERROR_MAX,YAW_ERROR_MIN=$YAW_ERROR_MIN ${CLUSTER_PATH}/experiment.slurm
     EXPERIMENT_COUNTER=$(($EXPERIMENT_COUNTER + 1))
 }
 
 for i in $(seq 1 $NUM_EXPERIMENTS); do
-    # params: reward_framewok, seed, algorithm, force_framework
-    submit_job "1" ${i} "sac" "1"
-    submit_job "2" ${i} "sac" "1"
-    submit_job "3" ${i} "sac" "1"
-    submit_job "4" ${i} "sac" "1"
-    submit_job "5" ${i} "sac" "1"
-    submit_job "6" ${i} "sac" "1"
+    # params: reward_framewok, seed, algorithm, force_framework, W_EPS_TORQUE, W_DELTA
+    submit_job "1" ${i} "sac" "1" "10" "1"
+    submit_job "1" ${i} "sac" "1" "8" "1"
+    submit_job "1" ${i} "sac" "1" "5" "1"
+    submit_job "1" ${i} "sac" "1" "1" "1"
+    
+    submit_job "1" ${i} "sac" "1" "8" "0.2"
+    submit_job "1" ${i} "sac" "1" "8" "0.7"
+    submit_job "1" ${i} "sac" "1" "8" "1.3"
+    submit_job "1" ${i} "sac" "1" "8" "2"
 done
 
 echo "Done."
