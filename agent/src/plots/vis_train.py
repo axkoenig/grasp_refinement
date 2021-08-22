0a714e0d776abb63f43c83a1bd001ac22357e46c
Alexander Koenig <awckoenig@gmail.com>
2021-08-23 01:47:05 +0200

    Make plots for more scalars

--- agent/src/plots/vis_train.py
+++ agent/src/plots/vis_train.py
@@ -13,12 +13,10 @@ sns.set(style="darkgrid", font_scale=1.5)
 OUTPUT_DIR = "./output"
 
 
-def get_scalar(
+def get_scalars(
     tb_log,
-    scalar_name,
-    window_size=10,
-    cap_len=99999,
-    every_10_step=False,
+    scalar_names,
+    window_size=25,
     smooth=False,
 ):
 
@@ -26,74 +24,46 @@ def get_scalar(
     event_acc.Reload()
 
     try:
-        _, time_steps, vals = zip(*event_acc.Scalars(scalar_name))
+        _, time_steps, vals = zip(*event_acc.Scalars(scalar_names))
     except KeyError:
-        print("Could not find scalar " + scalar_name)
+        print("Could not find scalar " + scalar_names)
         return None
 
     # convert time steps to episodes
-    time_steps = np.arange(1, len(time_steps) + 1)
-    time_steps = time_steps[:cap_len]
-    vals = vals[:cap_len]
-
-    # removes all data except multiples of 10, for faster plotting
-    if every_10_step:
-        new_time_steps = []
-        new_vals = []
-        for i in range(0, len(time_steps)):
-            if i % 10 == 0:
-                new_time_steps.append(i)
-                new_vals.append(vals[i])
-        time_steps = new_time_steps
-        vals = new_vals
+    episodes = np.arange(1, len(time_steps) + 1)
 
     # smooth data with moving average filter
-    if smooth or "sustained" in scalar_name:
+    if smooth or "sustained" in scalar_names:
         vals = np.convolve(vals, np.ones(window_size), "valid") / window_size
-        time_steps = np.convolve(time_steps, np.ones(window_size), "valid") / window_size
+        episodes = np.convolve(episodes, np.ones(window_size), "valid") / window_size
 
     # create pd dataframe
-    data = {"steps": time_steps, scalar_name: vals}
+    data = {"episodes": episodes, scalar_names: vals}
     return pd.DataFrame(data)
 
 
-def get_experiment_data(tb_log, scalar_name, plot_objs_individually):
+def get_experiment_data(tb_log, scalar_names):
 
     event_acc = EventAccumulator(tb_log)
     event_acc.Reload()
 
     # the variable that you want to keep fixed
-    try:
-        # _, _, x = zip(*event_acc.Scalars("hparams/learning_rate"))
-        # keep_fixed = 0.00019999999494757503
+    # try:
+    #     _, _, x = zip(*event_acc.Scalars("hparams/reward_framework"))
+    #     keep_fixed = 1
 
-        # if x[0] != keep_fixed:
-        #     print("none")
-        #     return None
+    #     if x[0] != keep_fixed:
+    #         print("none")
+    #         return None
 
-        # # the variable that you want to keep fixed
-        _, _, x = zip(*event_acc.Scalars("hparams/batch_size"))
-        keep_fixed = 256
-
-        if x[0] != keep_fixed:
-            print("none")
-            return None
-    except Exception as e:
-        print(e)
-        return None
+    # except Exception as e:
+    #     print(e)
+    #     return None
 
     df = pd.DataFrame()
-    if plot_objs_individually:
-        objs = [scalar_name + "_box", scalar_name + "_cylinder", scalar_name + "_sphere"]
 
-        for o in objs:
-            df = df.append(get_scalar(tb_log, o))
-    else:
-        if len(args.scalar_name) == 1:
-            df = get_scalar(tb_log, scalar_name)
-        else:
-            for s in args.scalar_name:
-                df = df.append(get_scalar(tb_log, s))
+    for s in scalar_names:
+        df = df.append(get_scalars(tb_log, s[0]))
 
     # get some interesting hyperparameters
     des_hparams = ["reward_framework", "force_framework", "w_eps_torque", "w_delta", "gradient_steps", "learning_rate", "batch_size", "tau"]
@@ -108,82 +78,77 @@ def get_experiment_data(tb_log, scalar_name, plot_objs_individually):
             force_framework = True if hparam == "force_framework" else False
             df[hparam] = get_framework_name(force_framework, x[0])
         else:
-            df[hparam] = x[0]
+            df[hparam] = f"{x[0]:.5f}"
+
+    # extract experiment id from log string
+    id = tb_log.split("id")[1].split("_")[0]
+    df["try_id"] = id
+    df["tb_log"] = tb_log
     return df
 
 
 def get_all_data(args):
+    print("Getting all data...")
     # get any directories that match the starting string
     dirs = [os.path.join(args.log_path, dir) for dir in os.listdir(args.log_path) if dir.startswith(args.prefix)]
 
     df = pd.DataFrame()
+
     for tb_log in dirs:
-        df = df.append(get_experiment_data(tb_log, args.scalar_name, args.plot_objs_individually))
+        print("Getting " + tb_log)
+        df = df.append(get_experiment_data(tb_log, args.scalar_names))
+
+    # for each experiment (via unique id) get maximum episode number and from those select the min
+    # results = []
+    # experiments = np.unique(df["tb_log"].values)
+    # for exp in experiments:
+    #     max_ep = df.loc[df["tb_log"] == exp, "episodes"].max()
+    #     results.append((exp, max_ep))
+    # sorted_results = sorted(results, key=lambda t: t[1])
+    # print(sorted_results)
+    # min_ep_over_all_exp = sorted_results[0][1]
+    # print("cutting all experiments at " + str(min_ep_over_all_exp))
+    # df = df[df.episodes <= min_ep_over_all_exp]
 
     print(df)
     return df
 
 
-def get_y_label(scalar):
-
-    if "sustained" in scalar:
-        return "Success Rate"
-    elif "ep_rew" in scalar:
-        return "Mean Episode Reward"
-    elif "actor" in scalar:
-        return "Actor Loss"
-    elif "critic" in scalar:
-        return "Critic Loss"
-    else:
-        return scalar
-
-
 def plot(args, df):
+    num_frameworks = len(df[args.compare].unique())
+    palette = sns.color_palette("tab10", num_frameworks)
 
-    palette = sns.color_palette("tab10", len(df[args.compare].unique()))
+    num_plots = len(args.scalar_names)
+    fig = plt.figure(figsize=(12, 17))
+    num_tries_per_exp = len(np.unique(df["tb_log"].values)) / num_frameworks
+    print("Num tries per exp " + str(num_tries_per_exp))
+    # if num_tries_per_exp % 1 != 0:
+    #     print("Num tries per exp should be an integer. it is " + str(num_tries_per_exp))
+    #     import pdb; pdb.set_trace()
+    fig.suptitle(f"Results averaged over {int(num_tries_per_exp)} seeds.")
 
-    if args.plot_objs_individually:
-        object_types = ["box", "cylinder", "sphere"]
-        num_object_types = len(object_types)
-        fig, ax = plt.subplots(1, num_object_types, figsize=(15, 5), sharey=True)
+    for i in range(num_plots):
+        ax = fig.add_subplot(4, 3, i + 1)
+        sns.lineplot(data=df, x="episodes", y=args.scalar_names[i][0], palette=palette, hue=args.compare, ax=ax)
 
-        for i in range(num_object_types):
+        lines, labels = ax.get_legend_handles_labels()
+        ax.get_legend().remove()
+        ax.set_xlabel("Num. of Episodes")
+        if "box" in args.scalar_names[i][0]:
+            ax.set_xlabel("Num. of Box-Episodes")
+        elif "cylinder" in args.scalar_names[i][0]:
+            ax.set_xlabel("Num. of Cylinder-Episodes")
+        elif "sphere" in args.scalar_names[i][0]:
+            ax.set_xlabel("Num. of Sphere-Episodes")
+        ax.set_ylabel(args.scalar_names[i][1])
+        if "sustained" in args.scalar_names[i][0]:
+            ax.set_ylim((0, 1))
 
-            ax = sns.lineplot(data=df, x="steps", y=args.scalar_name + "_" + object_types[i], palette=palette, hue=args.compare, legend="full")
-            ax[i].set_xlabel("Number of Episodes")
-            ax[i].set_ylabel(None)
-            if not i:
-                ax[i].set_ylabel(get_y_label(args.scalar_name))
-            ax[i].set_title(object_types[i].capitalize())
-            if "sustained" in args.scalar_name:
-                ax[i].set_ylim((0, 1))
-    else:
-
-        if len(args.scalar_name) == 1:
-            fig = plt.figure()
-            ax = sns.lineplot(data=df, x="steps", y=args.scalar_name, palette=palette, hue=args.compare, legend="full")
-            ax.set_title(f"Resuts over 4 seeds")
-            ax.set_xlabel("Number of Episodes")
-            ax.set_ylabel(args.scalar_name)
-        else:
-            
-            fig, ax = plt.subplots(
-                1,
-                len(args.scalar_name),
-                figsize=(15, 5),
-            )
-            for i in range(len(args.scalar_name)):
-                sns.lineplot(data=df, x="steps", y=args.scalar_name[i], palette=palette, hue=args.compare, legend="full", ax=ax[i])
-                ax[i].set_xlabel("Number of Episodes")
-                ax[i].set_ylabel(get_y_label(args.scalar_name[i]))
-                if "sustained" in args.scalar_name[i]:
-                    ax[i].set_ylim((0, 1))
-
-    fig.tight_layout(pad=0.5)
+    fig.tight_layout(rect=[0, 0, 1, 0.95], pad=0.5)
+    fig.legend(lines, labels, loc="upper right", ncol=2)
     fig.show()
-    args.scalar_name = "_".join(args.scalar_name)
-    args.scalar_name = args.scalar_name.replace("/", "_")
-    fig.savefig(f"{OUTPUT_DIR}/{args.compare}_{args.scalar_name}_{args.prefix}.png")
+    fig.savefig(f"{OUTPUT_DIR}/final_{args.prefix}_{args.compare}.png")
+    fig.savefig(f"{OUTPUT_DIR}/final_{args.prefix}_{args.compare}.pdf")
 
 
 if __name__ == "__main__":
@@ -191,36 +156,42 @@ if __name__ == "__main__":
     parser.add_argument(
         "--log_path",
         type=str,
-        default="/home/parallels/cluster_logs_sd3",
+        default="/home/parallels/cluster_logs_sd6",
         help="Path to tensorboard log.",
     )
     parser.add_argument(
         "--prefix",
         type=str,
-        default="21Jul_LearnRateB",
+        default="16Aug_Final",
         help="Prefix comment of your experiment.",
     )
     parser.add_argument(
-        "--scalar_name",
+        "--scalar_names",
         type=str,
-        default="rollout/ep_rew_mean",
+        default="step/sustained_holding",
         help="Which metric to plot.",
     )
     parser.add_argument(
         "--compare",
         type=str,
-        default="learning_rate",
+        default="force_framework",
         help="Which metric to compare.",
     )
-    parser.add_argument(
-        "--plot_objs_individually",
-        type=int,
-        default=0,
-        help="Whether to plot objects individually.",
-    )
-
     args = parser.parse_args()
 
-    args.scalar_name = ["train/actor_loss", "train/critic_loss", "rollout/ep_rew_mean"]
+    args.scalar_names = [
+        ("step/sustained_holding", "Holding Success"),
+        ("step/sustained_lifting", "Lifting Success"),
+        ("rollout/ep_rew_mean", "Mean Episonde Reward"),
+        ("step/sustained_holding_box", "Holding Success Box"),
+        ("step/sustained_holding_cylinder", "Holding Success Cylinder"),
+        ("step/sustained_holding_sphere", "Holding Success Sphere"),
+        ("step/a_delta_cur", r"$\delta_{cur}$"),
+        ("step/a_delta_task", r"$\delta_{task}$"),
+        ("step/sum_contact_forces", "Sum of Contact Force Magn."),
+        ("step/a_epsilon_force", r"$\epsilon_{f}$"),
+        ("step/a_epsilon_torque", r"$\epsilon_{\tau}$"),
+        ("step/num_contacts", "Num. of Contacts"),
+    ]
     df = get_all_data(args)
     plot(args, df)
